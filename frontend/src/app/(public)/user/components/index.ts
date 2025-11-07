9d6d9e92b2d49bb977f5b52578f36b15fdb36165
LilyDangww <huephuongdang143@gmail.com>
2025-11-07 13:22:36 +0700

    feat: api book detail, integrate api login, signup and list book

--- frontend/src/app/(public)/user/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AccountInfo } from "./AccountInfo";
-export { default as ChangePassword } from "./ChangePassword";
-export { default as RentedBooks } from "./RentedBooks";
-export { default as HoldsManagement } from "./HoldsManagement";
--- frontend/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-// Layout Components
-export { Header, Footer } from './layout';
-
-// UI Components
-export { Input } from './ui/input';
-
-// Page Components
-export { default as BookCard } from './BookCard';
-export { default as Pagination } from './Pagination';
